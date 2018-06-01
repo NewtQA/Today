7c9c03fad95b259a79849f75fd5cd240214788b9
NewtQA <alshas@newtglobal.com>
2018-06-01 10:45:06 +0530

    first commit

---
+++ src/test/java/com/mercurytours/Demo/Test.java
@@ -0,0 +1,12 @@
+package com.mercurytours.Demo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int a=0,b=5;
+		 int c=b/a;
+		 System.out.println("the value of c is "+ c);
+	}
+
+}
